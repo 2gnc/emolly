0e83e451edd8f9bf5079bbf482acc90822dd2804
tgnc <tgnc@yandex-team.ru>
2021-09-07 10:04:06 +0300

    lock orientation experiment

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { cn } from '@bem-react/classname';
 import { Layout, LayoutIllustrationTypes } from '../Layout/Layout';
 import { Main } from '../Main/Main';
@@ -8,6 +8,12 @@ import './App.css';
 const b = cn('App');
 
 export const App: React.FC = () => {
+  useEffect(() => {
+    if (Screen && (Screen as any).lockOrientation) {
+      (Screen as any).lockOrientation();
+    }
+  }, []);
+
   return (
     <div className={b()}>
       <Layout illustrationType={LayoutIllustrationTypes.TOP}>
