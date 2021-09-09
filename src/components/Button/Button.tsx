b88bdf91656bc5a4cda78f53ea22654c72332625
tgnc <tgnc@yandex-team.ru>
2021-09-09 20:25:15 +0300

    form styling

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,11 +8,12 @@ interface Props extends IWithCls {
     text: string; // контент кнопки
     onClick?: (e: React.MouseEvent) => void; // обработчик клика если компонент используется как кнопка
     link?: string; // урл если компонент должен являться ссылкой
+    disabled?: boolean;
 }
 
 const b = cn('Button');
 
-export const Button: React.FC<Props> = ({ text, onClick, link, cls }) => {
+export const Button: React.FC<Props> = ({ text, onClick, link, cls, disabled = false }) => {
     if (link) {
         return (
             <a href={link} className={b({ isLink: true }, [cls])}>
@@ -21,7 +22,7 @@ export const Button: React.FC<Props> = ({ text, onClick, link, cls }) => {
         )
     } else if (onClick) {
         return (
-            <button onClick={onClick} className={b(null, [cls])}>
+            <button onClick={onClick} className={b(null, [cls])} disabled={disabled}>
                 {text}
             </button>
         )
--- src/components/EmailForm/EmailForm.tsx
+++ src/components/EmailForm/EmailForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { cn } from '@bem-react/classname';
 import { Button } from '../Button/Button';
 
@@ -9,14 +9,61 @@ interface Props {}
 const b = cn('EmailForm');
 
 export const EmailForm: React.FC<Props> = () => {
-const handleFormSubmit = (e: React.MouseEvent) => {
-    e.preventDefault();
-}
+    const handleFormSubmit = (e: React.MouseEvent) => {
+        e.preventDefault();
+    };
+
+    const [isUsernameInputValid, setUsernameInputValid] = useState<boolean | null>(true);
+    const [isEmailInputValid, setEmailInputValid] = useState<boolean | null>(true);
+    const [usernameInputContent, setUsernameInputContent] = useState('');
+    const [emailInputContent, setEmailInputContent] = useState('');
+    const [isAgreementChecked, setAgreementChecked] = useState(false);
+
+    const isButtonEnabled = Boolean(isUsernameInputValid !== false &&
+        isEmailInputValid !== false &&
+        usernameInputContent.length &&
+        emailInputContent.length &&
+        isAgreementChecked);
+
     return (
         <form className={b()}>
+            <label htmlFor="username">
+                Имя
+                <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    value={usernameInputContent}
+                    onChange={(e) => setUsernameInputContent(e.target.value)}
+                />
+            </label>
+            <label htmlFor="email">
+                E-mail
+                <input
+                    type="text"
+                    name="email"
+                    id="email"
+                    value={emailInputContent}
+                    onChange={(e) => setEmailInputContent(e.target.value)}
+                />
+            </label>
+            <label htmlFor="confirm">
+                <input
+                    type="checkbox"
+                    name="confirm"
+                    id="confirm"
+                    checked={isAgreementChecked}
+                    onChange={() => setAgreementChecked(!isAgreementChecked)}
+                    className={b('CheckboxInput')}
+                />
+                <div className={b('Checkbox')} />
+                Согласие на обработку персональных данных
+            </label>
             <Button
                 text="Отправить"
                 onClick={handleFormSubmit}
+                disabled={!isButtonEnabled}
+                cls={b('Button')}
             />
         </form>
     );
